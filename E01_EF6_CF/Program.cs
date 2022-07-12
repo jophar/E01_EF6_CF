8660123cb87eaaf3046f2c10c274c5134e08a87d
João Mariz <joao@joaomariz.eu>
2022-07-12 20:06:00 +0100

    Add project files.

---
+++ E01_EF6_CF/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E01_EF6_CF
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
